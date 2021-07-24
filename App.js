77cbed14f4427d8c358434f35cdc47c7dab56380
TimBlanchard <36016548+TimBlanchard@users.noreply.github.com>
2021-07-24 15:40:21 +0200

    add routing

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Route } from "wouter";
+import ListingSchool from "./src/pages/ListingSchools";
+import SingleSchool from "./src/pages/SingleSchool";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Heho</Text>
+      <Route path="/" component={ListingSchool}/>
+      <Route path="/schools/:name" component={SingleSchool}/>
       <StatusBar style="auto" />
     </View>
   );
--- src/pages/ListingSchools/index.js
+++ src/pages/ListingSchools/index.js
@@ -1,51 +1,12 @@
 import React from 'react';
-import Summary from '../../components/organisms/Summary';
-import SchoolRevenue from "../../components/organisms/SchoolRevenue";
 import './index.scss'
+import {View, Text} from "react-native";
 
-export default function SingleSchool() {
-    const array = [
-        {
-            id: 1,
-            title: 'Nbr. de distrib.',
-            values: {
-                number: 21
-            },
-            type: 'distributeur'
-        },
-        {
-            id: 2,
-            title: 'Le + utilisé',
-            period: '/1 semaine',
-            values: {
-                number: 138,
-                symbole: '#',
-                position: 'prefix'
-            },
-            type: 'users'
-        },
-        {
-            id: 3,
-            title: 'Le + rentable',
-            period: '/1 semaine',
-            values: {
-                number: 45,
-                symbole: '#',
-                position: 'prefix',
-                detail: '543€'
-            },
-            type: 'dollar'
-        }
-    ]
+export default function ListingSchool() {
 
     return (
-        <div className="main-app listing-school">
-            <div className="principal">
-                <Summary array={array} />
-            </div>
-            <aside>
-                <SchoolRevenue/>
-            </aside>
-        </div>
+        <View>
+            <Text>Bah ouais</Text>
+        </View>
     )
 }
