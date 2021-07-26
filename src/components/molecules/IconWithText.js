8a846c4db5090b32cd6ccc631f61dad48e0e5bf7
TimBlanchard <36016548+TimBlanchard@users.noreply.github.com>
2021-07-26 22:08:03 +0200

    add iconWithText molecule

---
+++ src/components/molecules/IconWithText.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet, Image} from "react-native";
+import styled from "styled-components/native";
+import { Ionicons } from '@expo/vector-icons';
+import {border_radius, box_shadow, color, letterSpacing, fontSize, neutral, spacing} from "../../styles/const";
+
+export default function IconWithText(props) {
+
+    const Container = styled.View`
+        flex-direction: row;
+      align-items: center;
+      padding: ${spacing[5]};
+      justify-content: center;
+  
+`;
+
+    const Txt = styled.Text`
+    color: ${props.textColor ? props.textColor : neutral.black};
+      font-size: ${ props.size_small ? fontSize[3] : fontSize[5]};
+      letter-spacing: ${letterSpacing.neg_3};
+      margin-right: ${props => props.value ? spacing[2] : 0 };
+      font-family: ${props => props.value ? 'Moderat Bold' : 'Moderat' };
+  `;
+
+    const styles = StyleSheet.create({
+        icon: {
+            width: 12,
+            height: 12,
+            marginRight: spacing[2],
+            resizeMode: 'stretch'
+        },
+    })
+
+
+    return (
+        <Container>
+            <Image style={styles.icon} source={require(`../../../assets/${props.iconName}.png`)} />
+            {
+                props.value &&
+                <Txt value>{props.value}</Txt>
+            }
+            <Txt>{props.text}</Txt>
+        </Container>
+    )
+}
--- src/components/organisms/SingleSchoolHeader.js
+++ src/components/organisms/SingleSchoolHeader.js
@@ -6,13 +6,16 @@ import schoolsData from "../../helpers/schoolsData";
 import ListingSchoolItem from "../molecules/ListingSchoolItem";
 import Title from "../atoms/Title";
 import InputSearch from "../molecules/InputSearch";
+import IconWithText from "../molecules/IconWithText";
 
 
 export default function SingleSchoolHeader(props) {
 
     return (
         <Container>
-            <Title text={props.name} large />
+            <IconWithText text='Etablissement' iconName='localisation' textColor={color.primary} size_small/>
+            <IconWithText text='Etablissement' iconName='localisation-black' value={8}/>
+            <Title text={props.name} large center />
             <InputSearch secondary placeholder="Rechercher un produit ou une catégorie" />
         </Container>
     )
